bf4da0fea413f7f7516a19c72f81a78d982d4058
YavuzSelimGugen <ygugen@windowslive.com>
2019-04-05 15:36:38 +0300

    Update Expression.js

--- CW5/Expression.js
+++ CW5/Expression.js
@@ -6,57 +6,58 @@ let operCount = -1;
 
 
 function match(k) {
-    if (tok.kind == k) 
+    if (tok.kind == k)
         tok = tokens.pop();
     else expected(k);
 }
 function expected(s) {
-    error(s+" expected -- "+tok+" found");
+    error(s + " expected -- " + tok + " found");
 }
 function error(s) {
-    throw ("At index "+tok.index+": "+s);
+    throw ("At index " + tok.index + ": " + s);
 }
 function showError(elt) {
     elt.selectionStart = tok.index
     elt.selectionEnd = tok.index + tok.length
-    elt.focus(); 
+    elt.focus();
 }
 
 class Constant {
-   constructor(num) { this.num = num; }
-   fValue() { return this.num; }
-   toTree(param) { return space.repeat(param) + this.num + '\n' }
-   toPostfix() { return this.num+' '; }
-   toString() { return this.num.toString(); }
+    constructor(num) { this.num = num; }
+    fValue() { return this.num; }
+    toTree(param) { return space.repeat(param) + this.num + '\n' }
+    toPostfix() { return this.num + ' '; }
+    toString() { return this.num.toString(); }
 }
 class Binary {
-   constructor(left, oper, right) {
-      this.left = left; this.oper = oper; this.right = right;
-   }
-   fValue() {
-      switch (this.oper) { 
-      case PLUS:  return this.left.fValue()+this.right.fValue();
-      case MINUS: return this.left.fValue()-this.right.fValue();
-      case STAR:  return this.left.fValue()*this.right.fValue();
-      case SLASH: 
-         let v = this.right.fValue();
-         if (v == 0) 
-            throw ("Division by zero");
-         return this.left.fValue()/v;
-      default: return NaN;
-      }
-   }
-   toTree() {
-       //return this.oper+'\n'+this.left.toTree()+this.right.toTree()
-      return space.repeat(++operCount) + this.oper + '\n' 
-        + this.left.toTree(operCount) + this.right.toTree(operCount--)      
-   }
-   toPostfix() {
-      return this.left.toPostfix()+this.right.toPostfix()+this.oper+' '
-   }
-   toString() {
-      return '('+this.left + this.oper + this.right+')'
-   }
+    constructor(left, oper, right) {
+        this.left = left; this.oper = oper; this.right = right;
+    }
+    fValue() {
+        switch (this.oper) {
+            case POWER: return Math.pow(this.left.fValue(), this.right.fValue());
+            case PLUS: return this.left.fValue() + this.right.fValue();
+            case MINUS: return this.left.fValue() - this.right.fValue();
+            case STAR: return this.left.fValue() * this.right.fValue();
+            case SLASH:
+                let v = this.right.fValue();
+                if (v == 0)
+                    throw ("Division by zero");
+                return this.left.fValue() / v;
+            default: return NaN;
+        }
+    }
+    toTree() {
+        //return this.oper+'\n'+this.left.toTree()+this.right.toTree()
+        return space.repeat(++operCount) + this.oper + '\n'
+            + this.left.toTree(operCount) + this.right.toTree(operCount--)
+    }
+    toPostfix() {
+        return this.left.toPostfix() + this.right.toPostfix() + this.oper + ' '
+    }
+    toString() {
+        return '(' + this.left + this.oper + this.right + ')'
+    }
 }
 
 function binary(e) {
@@ -64,10 +65,10 @@ function binary(e) {
     return new Binary(e, op, term());
 }
 function expression() {
-    let e = (tok.kind == MINUS)?
-      binary(new Constant(0)) : term();
-    while (tok.kind == PLUS || tok.kind == MINUS) 
-      e = binary(e);
+    let e = (tok.kind == MINUS) ?
+        binary(new Constant(0)) : term();
+    while (tok.kind == PLUS || tok.kind == MINUS)
+        e = binary(e);
     return e;
 }
 function term() {
@@ -79,16 +80,16 @@ function term() {
     return e;
 }
 function factor() {
-    switch (tok.kind)  {
-    case NUMBER:
-      let c = tok.val;
-      match(NUMBER);
-      return new Constant(c);
-    case LEFT:
-      match(LEFT); 
-      let e = expression();
-      match(RIGHT); return e;
-    default: expected("Factor");
+    switch (tok.kind) {
+        case NUMBER:
+            let c = tok.val;
+            match(NUMBER);
+            return new Constant(c);
+        case LEFT:
+            match(LEFT);
+            let e = expression();
+            match(RIGHT); return e;
+        default: expected("Factor");
     }
     return null;
 }
